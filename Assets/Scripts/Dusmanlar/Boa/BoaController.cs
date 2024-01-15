e7db74e4ab6bbb85f0b5999f7086463ce24cb6b4
keremkkucukk <keremkkucukk@gmail.com>
2024-01-15 19:29:15 +0300

    +boa animation-animator +boaScript

---
+++ Assets/Scripts/Dusmanlar/Boa/BoaController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoaController : MonoBehaviour
+{
+    [SerializeField]
+    float boaYurumeHizi, boaKosmaHizi;
+
+    Animator anim;
+    Rigidbody2D rb;
+}
